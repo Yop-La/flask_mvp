824ecc62766eb06b895e7dac76ec71f0c803aabc
Yop-La <alex.guilleminegmail.com>
2023-04-13 21:24:34 +0200

    add user model

---
+++ app/models/user.py
@@ -0,0 +1,33 @@
+
+from db import db
+
+
+class UserModel(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    is_admin = db.Column(db.Boolean)
+
+    @classmethod
+    def find_by_username(cls, username: str) -> "UserModel":
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls, _id: int) -> "UserModel":
+        return cls.query.filter_by(id=_id).first()
+
+    def save_to_db(self) -> None:
+        db.session.add(self)
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
+
+    def delete_from_db(self) -> None:
+        db.session.delete(self)
+        try:
+            db.session.commit()
+        except:
+            db.session.rollback()
